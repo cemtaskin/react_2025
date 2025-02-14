fd415ed581a1e593ba417e86c3a7937de121131f
cemtaskin82@gmail.com <cemtaskin82@gmail.com>
2025-02-14 12:11:55 +0300

    Expense Item usinf props

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,7 +1,23 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  return <h2 style={{ color: "blue" }}>Expense Item !</h2>;
+function ExpenseItem({ date, amount, title }) {
+  const month = date.toLocaleDateString("en-US", { month: "long" });
+  const day = date.toLocaleDateString("en-US", { day: "2-digit" });
+  const year = date.getFullYear();
+
+  return (
+    <div className="expense-item">
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
+      <div className="expense-item__description">
+        <div>{title}</div>
+        <div className="expense-item__price">${amount.toFixed(2)}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
